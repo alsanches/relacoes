45ab638a5c1577d841877b750bc991acbba93c6a
Alexandre Sanches <alsanches@gmail.com>
2016-04-07 12:28:15 -0300

    relaticoes

---
+++ database/migrations/2016_04_07_103056_create_views_post.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateViewsPost extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views_post', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_posts')->unsigned();
+            $table->foreign('id_posts')->references('id')->on('posts')->onDelete('cascade');
+            $table->integer('qtd');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       Schema::drop('views_post');
+    }
+}
